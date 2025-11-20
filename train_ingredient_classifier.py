b96735cc79ee734a6f86dc83f8b06c376f736772
Zachary <zacharyfitz22@gmail.com>
2025-11-20 08:42:56 -0500

    Speed up processing

--- train_ingredient_classifier.py
+++ train_ingredient_classifier.py
@@ -76,8 +76,8 @@ def main():
 
     training_args = TrainingArguments(
         output_dir=MODEL_OUT,
-        per_device_train_batch_size=16,
-        per_device_eval_batch_size=16,
+        per_device_train_batch_size=4,
+        per_device_eval_batch_size=4,
         eval_strategy="epoch",
         save_strategy="epoch",
         num_train_epochs=2,
